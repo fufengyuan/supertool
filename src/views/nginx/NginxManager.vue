f8526c23b3fd01d04d3547a300bf1dbcbdf975fd
duormi <312848220@qq.com>
2026-05-14 13:53:31 +0800

    fix: use v-if instead of v-show for tab content to prevent all components mounting at once

--- src/views/nginx/NginxManager.vue
+++ src/views/nginx/NginxManager.vue
@@ -121,7 +121,12 @@
 
         <!-- 选项卡内容 -->
         <div class="bg-base-100 border border-base-content/10 rounded-xl flex-1 min-h-[400px]">
-          <div v-for="tab in tabs" :key="tab.key" v-show="currentTab === tab.key" class="p-4">
+          <div
+            v-for="tab in tabs"
+            :key="tab.key"
+            v-if="currentTab === tab.key"
+            class="p-4"
+          >
             <!-- 未选择预设时显示提示 -->
             <div v-if="!currentPreset" class="flex items-center justify-center h-32 text-base-content/50">
               <p>请先选择一个预设</p>
